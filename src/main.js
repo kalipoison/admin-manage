a3e211f189ae81a933ecfb81b83bd541fdd57c13
kalipoison <1174616074@qq.com>
2021-12-02 00:41:27 +0800

    vue-cli删除不必要代码和组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 import './plugins/element.js'
 
 Vue.config.productionTip = false
